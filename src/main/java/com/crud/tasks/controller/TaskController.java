039d1db7feb43744e956421ac8dc9bac72cc13c9
Makedo81 <domi.stan@wp.pl>
2019-03-09 20:24:29 +0100

    Task 19.2

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,47 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @Autowired
+    public DbService dbService;
+    @Autowired
+    TaskMapper taskMapper;
+
+    @RequestMapping(method = RequestMethod.GET,value = "getTasks")
+    public List<TaskDto> getTasks(){
+        return taskMapper.mapToListDtoList(dbService.getAllTasks());
+    }
+
+    @RequestMapping(method = RequestMethod.GET,value = "getTask")
+    public TaskDto getTask(@RequestParam Long id){
+        return taskMapper.mapToTaskDto(dbService.getTask(id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "createTask",consumes = APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto){
+         dbService.saveTask(taskMapper.mapToTask(taskDto));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "updateTask")
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
+        return taskMapper.mapToTaskDto(dbService.saveTask(taskMapper.mapToTask(taskDto)));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "deleteTask")
+    public void deleteTask(@RequestParam Long id){
+        dbService.deleteTask(id);
+    }
+
+}
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.service;
+
+//import com.crud.tasks.controller.TaskNotFoundException;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class DbService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task>getAllTasks(){
+        List<Task>a = repository.findAll();
+        System.out.println(a.size());
+        return a;
+    }
+
+    public Task getTask(final Long id){
+        return repository.findOne(id);
+    }
+
+    public Task saveTask(Task task){
+        return  repository.save(task);
+    }
+
+    public void deleteTask(final Long id){ repository.delete(id);}
+
+
+}
