21669b609a6c9d9f1b2bfa1a5ed3e765a8b12b78
Makedo81 <domi.stan@wp.pl>
2019-04-09 06:25:38 +0200

    tasks modul 23

--- src/main/java/com/crud/tasks/controller/TrelloControler.java
+++ src/main/java/com/crud/tasks/controller/TrelloControler.java
@@ -3,6 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,34 +18,23 @@ public class TrelloControler {
     @Autowired
     private TrelloClient trelloClient;
 
+    @Autowired
+    private TrelloService trelloService;
+
     @RequestMapping(method = RequestMethod.GET,value = "getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-//        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId()
-//                    + " " + trelloBoardDto.getName()));
-
-        trelloClient.getTrelloBoards().stream()
-                .filter(b->b.getName().contains("Kodilla"))
-                .forEach(b->System.out.println(b.getId()+ " " +b.getName()));
-
-        // GET request
-       // List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-      //  trelloBoards = trelloClient.getTrelloBoards();
-
-//        trelloBoards.forEach(trelloBoardDto -> {
-//
-//            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-//
-//            System.out.println("This board contains lists: ");
-//
-//            trelloBoardDto.getLists().forEach(trelloList ->
-//                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-//
-//        });
-        return trelloClient.getTrelloBoards();
+        return trelloService.fetchTrelloBoards();
     }
+
+//    @RequestMapping(method = RequestMethod.GET,value = "getTrelloBoards")
+//    public List<TrelloBoardDto> getTrelloBoards() {
+//
+//        trelloClient.getTrelloBoards().stream()
+//                .filter(b->b.getName().contains("Kodilla"))
+//                .forEach(b->System.out.println(b.getId()+ " " +b.getName()));
+//
+//        return trelloClient.getTrelloBoards();
+//    }
 
     @RequestMapping(method = RequestMethod.POST,value = "createTrelloCard" )
     public CreatedTrelloCard createTrelloCard (@RequestBody TrelloCardDto trelloCardDto) {
@@ -52,3 +42,27 @@ public class TrelloControler {
     }
 }
 
+
+
+
+
+
+//    List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId()
+//                    + " " + trelloBoardDto.getName()));
+//
+//        trelloClient.getTrelloBoards().stream()
+//                .filter(b->b.getName().contains("Kodilla"))
+//                .forEach(b->System.out.println(b.getId()+ " " +b.getName()));
+
+// GET request
+// List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//  trelloBoards = trelloClient.getTrelloBoards();
+
+//        trelloBoards.forEach(trelloBoardDto -> {
+//            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+//            System.out.println("This board contains lists: ");
+//            trelloBoardDto.getLists().forEach(trelloList ->
+//                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+//
+//        });
---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleEmailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void send(final Mail mail) {
+
+        LOGGER.info("Starting email preparation");
+    try {
+        SimpleMailMessage mailMessage = createMessage(mail);
+        javaMailSender.send(mailMessage);
+        LOGGER.info("Email has been sent");
+    }
+    catch (MailException e) {
+        LOGGER.error("Fail to process email sending",e.getMessage(),e);
+        }
+    }
+    public SimpleMailMessage createMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        if (mail.getToCc() != "") {
+            mailMessage.setCc(mail.getToCc());
+        }
+        return mailMessage;
+    }
+}
